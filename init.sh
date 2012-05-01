7da00c4ed6dc9aa88c82735a6fef560cd8b41909
Jay Greene <rufusthedeadcat@gmail.com>
2012-05-01 10:43:27 -0600

    Added init script

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIRECTORY=$(cd `dirname $0` && pwd)
+
+ln -s $@ $DIRECTORY/.vimrc $HOME/.vimrc
+ln -s $@ $DIRECTORY/.bashrc $HOME/.bashrc
+ln -s $@ $DIRECTORY/.vim $HOME/.vim
+ln -s $@ $DIRECTORY/.bash_aliases $HOME/.bash_aliases
+ln -s $@ $DIRECTORY/.pythonrc $HOME/.pythonrc
+
+# setup git global settings
+git config --global alias.st  status
+git config --global alias.ci  commit
+git config --global alias.co  checkout
+git config --global alias.ls  ls-files
