e3aca3002f0bbc32b1cc8df5618279e01cc102d1
Jay Greene <rufusthedeadcat@gmail.com>
2012-05-04 13:43:39 -0600

    Renamed files and altered init.sh so Linux will stop hiding them.

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@
 
 DIRECTORY=$(cd `dirname $0` && pwd)
 
-ln -s $@ $DIRECTORY/.vimrc $HOME/.vimrc
-ln -s $@ $DIRECTORY/.bashrc $HOME/.bashrc
-ln -s $@ $DIRECTORY/.vim $HOME/.vim
-ln -s $@ $DIRECTORY/.bash_aliases $HOME/.bash_aliases
-ln -s $@ $DIRECTORY/.pythonrc $HOME/.pythonrc
+#ln -s $@ $DIRECTORY/vimrc $HOME/.vimrc
+ln -s $@ $DIRECTORY/bashrc $HOME/.bashrc
+#ln -s $@ $DIRECTORY/vim $HOME/.vim
+ln -s $@ $DIRECTORY/bash_aliases $HOME/.bash_aliases
+#ln -s $@ $DIRECTORY/pythonrc $HOME/.pythonrc
 
 # setup git global settings
 git config --global alias.st  status
